e746c0e3fb90c0dc61cab7385bea1fecddbb6a56
Hanes-smart <hanesharinins2005@gmail.com>
2025-04-30 23:45:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,16 +1,16 @@
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        nums = min(len(s) for s in strs)
-        num = strs[0][:nums] 
-        for i in range(1,len(strs)):
-            count = ""
-            for j in range(len(num)):
-                if strs[i][j] == num[j]:
-                    count+=  num[j]
-                else:
-                    break
-            num =count
-        return num
-
-S= Solution()
-print(S.longestCommonPrefix(["flower","flow","flight"]))
+        nums= sorted(strs)
+        num = nums[0]
+        count = ""
+        for i in range(len(num)):
+            if num is None:
+                return count
+            if len(strs) is 1:
+                return strs[0]
+            if num[i] == nums[1][i]:
+                count+= num[i]
+            else:
+                break
+        return count
