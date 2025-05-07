804e3553979d37399deef08d4a9e42390e25c837
Hanes-smart <hanesharinins2005@gmail.com>
2025-05-08 00:37:40 +0530

    Time: 3 ms (57.67%), Space: 23.3 MB (82.21%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -9,6 +9,23 @@ class Solution:
         if root is None: return 0
 
         return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+
+    '''
+        def lnode(node): # not a class method
+            if not node: return 0
+            return 1 + lnode(node.left)
+
+        def rnode(node):
+            if not node: return 0
+            return 1 + rnode(node.right)
+
+        l,r = lnode(root), rnode(root)
+        
+        if l > r:
+            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+        else:
+            (2**l) -1 #height of the prefect tree
+            '''
        
 
 
