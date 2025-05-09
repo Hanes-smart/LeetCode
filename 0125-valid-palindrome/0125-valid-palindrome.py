adaf963c2d37436ae501e529772a79c6cb6321f8
Hanes-smart <hanesharinins2005@gmail.com>
2025-05-09 13:48:56 +0530

    Time: 19 ms (9.07%), Space: 18 MB (69.69%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,11 @@
-import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        res =""
-        cleaned_text = re.sub(r'[^A-Za-z0-9]', '', s)
-        lower = cleaned_text.lower()
-        if lower == lower[::-1] :
-            return True
-        else:
-            return False
+        res = ""
+        for i in range(len(s)):
+            if 48 <= ord(s[i]) <= 57 or 97 <= ord(s[i]) <= 122:
+                res += s[i]
+            elif 65 <= ord(s[i]) <= 90:
+                res += s[i].lower()
+        return res == res[::-1]
+
 
